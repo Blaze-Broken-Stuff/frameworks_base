a3918a471d4145338d88092d7fc46388c31baeae
Dmitrii <bankersenator@gmail.com>
2022-06-07 11:02:00 +0200

    core: Adapt biometrics code for June merge
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Change-Id: Iaa9eed4b8f8756c3cd4c26bc69fb0d5370127de2

--- services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
+++ services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
@@ -266,7 +266,7 @@ public class BiometricScheduler {
         if (mCurrentOperation != null) {
             if(mCancel) {
                Slog.v(getTag(), "Not idle, cancelling current operation: " + mCurrentOperation);
-               cancelInternal(mCurrentOperation);
+               mCurrentOperation.cancel(mHandler, mInternalCallback);
             } else {
                Slog.v(getTag(), "Not idle, current operation: " + mCurrentOperation);
                return;
